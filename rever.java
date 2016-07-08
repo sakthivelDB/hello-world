b443695db55f6d9b1dedbb1e1094559eebd254da
sakthivelDB <sakthivelatkpriet@gmail.com>
2016-07-08 14:49:00 +0530

    Add files via upload

---
+++ rever.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class rever
+{
+public static void main(String[]args)throws IOException
+{
+String s="ans";
+String reverse = new StringBuffer(s).reverse().toString();
+System.out.println(reverse);
+}
+}
